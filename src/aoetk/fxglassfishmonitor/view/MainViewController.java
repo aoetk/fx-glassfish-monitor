0fdb172302a42b39de77c8eb062dc395aa88a3f2
AOE Takashi <aoe1219@gmail.com>
2013-05-07 00:10:36 +0900

    初期描画処理のスケルトンコードを作成。

--- src/aoetk/fxglassfishmonitor/view/MainViewController.java
+++ src/aoetk/fxglassfishmonitor/view/MainViewController.java
@@ -2,8 +2,12 @@ package aoetk.fxglassfishmonitor.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import aoetk.fxglassfishmonitor.model.GlassFishMonitor;
+import aoetk.fxglassfishmonitor.model.ResourceHolder;
+import aoetk.fxglassfishmonitor.serviceclient.ConnectFailedException;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -33,11 +37,22 @@ public class MainViewController extends DraggableViewBase implements Initializab
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         containerPane.setCursor(Cursor.OPEN_HAND);
+        monitor = new GlassFishMonitor();
+        try {
+            monitor.initialize();
+            drawView(monitor.getServerResource());
+        } catch (ConnectFailedException ex) {
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
     void handleBtnExitAction(ActionEvent event) {
         Platform.exit();
+    }
+
+    private void drawView(ResourceHolder rootResource) {
+        // TODO 描画処理
     }
 
 }
